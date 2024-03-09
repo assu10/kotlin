04fde53afd0bcd793586bffd3812b33af690339d
assu10 <juhyun10.lee@gmail.com>
2024-03-09 12:17:18 +0900

    first

---
+++ src/main/kotlin/assu/study/kotlin_me/KotlinMeApplication.kt
@@ -0,0 +1,11 @@
+package assu.study.kotlin_me
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinMeApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinMeApplication>(*args)
+}
---
+++ src/test/kotlin/assu/study/kotlin_me/KotlinMeApplicationTests.kt
@@ -0,0 +1,13 @@
+package assu.study.kotlin_me
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinMeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
