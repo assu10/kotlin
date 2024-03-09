d1a0eea296fd16894c3f4f17dfe6cd8809fdd75f
assu10 <juhyun10.lee@gmail.com>
2024-03-09 15:42:28 +0900

    Kotlin - 코틀린 기본

---
+++ src/main/kotlin/assu/study/kotlin_me/helloWorld/Numbers.kt
@@ -0,0 +1,15 @@
+package assu.study.kotlin_me.helloWorld
+
+fun main() {
+    val i: Int = Int.MAX_VALUE
+    val l: Long = Long.MAX_VALUE
+    val a: Int = 2_147_483_647  // 가독성을 위해
+
+    println(i)  // 2147483647
+    println(i + 1)  // -2147483648
+    println(l + 1)  // -9223372036854775808
+
+    val aa = 1000   // Int
+    val bb = 1000L  // Long
+    val cc = 1000.0 // Double
+}
