fa989443b16cf04ca1f1eb2111b81b22f3e270fe
assu10 <juhyun1010@naver.com>
2024-09-18 17:29:50 +0900

    .

--- src/main/kotlin/assu/study/kotlinme/chap04/memberReference/TopLevelFunctionRef.kt
+++ src/main/kotlin/assu/study/kotlinme/chap04/memberReference/TopLevelFunctionRef.kt
@@ -25,6 +25,7 @@ fun main() {
     val message =
         listOf(
             Message3("Assu", "gogo!", false, listOf()),
+            Message3("Assu2", "gogo!2", false, listOf()),
             Message3(
                 "Assu",
                 "gogo!",
@@ -37,12 +38,18 @@ fun main() {
 
     // 최상위 수준 함수에 대한 참조 전달
     val result1 = message.filter(::ignore)
+
+    // `count()` 대신 size 를 사용하는 비효율적인 코드
     val result2 = message.filter(::ignore).size
+    val result3 = message.count(::ignore)
+
     println(result1) // [Message3(sender=Assu, text=gogo!, isRead=false, attachments=[])]
     println(result2) // 1
+    println(result3) // 1
 
-    val result3 = message.filterNot(::ignore)
-    val result4 = message.filterNot(::ignore).size
-    println(result3) // [Message3(sender=Assu, text=gogo!, isRead=false, attachments=[Attachment3(type=image, name=cute dog)])]
-    println(result4) // 1
+    val result4 = message.filterNot(::ignore)
+    val result5 = message.filterNot(::ignore).size
+
+    println(result4) // [Message3(sender=Assu, text=gogo!, isRead=false, attachments=[Attachment3(type=image, name=cute dog)])]
+    println(result5) // 1
 }
