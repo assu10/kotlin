38b60279c2ac9293830868371d39a0100b8a2e1b
assu10 <juhyun1010@naver.com>
2024-09-19 18:11:23 +0900

    vararg

--- src/main/kotlin/assu/study/kotlinme/chap02/VarargLikeList.kt
+++ src/main/kotlin/assu/study/kotlinme/chap02/VarargLikeList.kt
@@ -5,7 +5,31 @@ fun evaluate(vararg ints: Int) =
         "Sum: ${ints.sum()}\n" +
         "Average: ${ints.average()}\n"
 
+fun evaluate2(ints: List<Int>) =
+    "Size: ${ints.size}\n" +
+        "Sum: ${ints.sum()}\n" +
+        "Average: ${ints.average()}\n"
+
 fun main() {
-    var result = evaluate(1, -1, 1, 2, 3)
-    println(result)
+    val result1 = evaluate(1, -1, 1, 2, 3)
+
+    // Size: 5
+    // Sum: 6
+    // Average: 1.2
+    println(result1)
+
+    // 컴파일 오류
+//    val result2 = evaluate2(1, -1, 1, 2, 3)
+//    println(result2)
+
+    // 컴파일 오류
+//    val result3 = evaluate(listOf(1, -1, 1, 2, 3))
+//    println(result3)
+
+    val result4 = evaluate2(listOf(1, -1, 1, 2, 3))
+
+    // Size: 5
+    // Sum: 6
+    // Average: 1.2
+    println(result4)
 }
